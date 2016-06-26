fd9df7afdaf11e9dae39a6252c80266773da991a
bugtsa <bugtsa@gmail.com>
2016-06-26 12:09:03 +0300

    add shared prefences

---
+++ app/src/main/java/com/softdesign/devintensive/data/managers/DataManager.java
@@ -0,0 +1,23 @@
+package com.softdesign.devintensive.data.managers;
+
+public class DataManager {
+
+    private static DataManager INSTANCE = null;
+
+    public PreferencesManager mPreferencesManager;
+
+    public DataManager() {
+        mPreferencesManager = new PreferencesManager();
+    }
+
+    public static DataManager getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new DataManager();
+        }
+        return INSTANCE;
+    }
+
+    public PreferencesManager getPreferencesManager() {
+        return mPreferencesManager;
+    }
+}
--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -2,27 +2,44 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.data.managers.DataManager;
 import com.softdesign.devintensive.utils.ConstantManager;
 import com.softdesign.devintensive.utils.LogUtils;
+import com.softdesign.devintensive.utils.ToastUtils;
 
-public class MainActivity extends AppCompatActivity{
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = ConstantManager.TAG_PREFIX + MainActivity.class.getSimpleName();
 
-    protected int mColorMode;
-
     private CoordinatorLayout mCoordinatorLayout;
+
+    private DataManager mDataManager;
+
+    private EditText mUserPhone, mUserEmail, mUserVK, mUserGit, mUserBio;
+
+    private List<EditText> mUserInfoViews;
 
     private Toolbar mToolbar;
 
     private DrawerLayout mNavigationDrawer;
+
+    private FloatingActionButton mFab;
+
+    private int mCurrentEditMode = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +47,37 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         LogUtils.d(TAG, "onCreate");
 
+        mDataManager = DataManager.getInstance();
+
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_layout);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mUserPhone = (EditText) findViewById(R.id.phone_et);
+        mUserEmail = (EditText) findViewById(R.id.email_et);
+        mUserVK = (EditText) findViewById(R.id.vk_profile_et);
+        mUserGit = (EditText) findViewById(R.id.repo_et);
+        mUserBio = (EditText) findViewById(R.id.about_me_et);
+
+        mUserInfoViews = new ArrayList<>();
+        mUserInfoViews.add(mUserPhone);
+        mUserInfoViews.add(mUserEmail);
+        mUserInfoViews.add(mUserVK);
+        mUserInfoViews.add(mUserGit);
+        mUserInfoViews.add(mUserBio);
+
+        mFab.setOnClickListener(this);
         setupToolBar();
+        setupDrawer();
+        loadUserInfoValue();
+
+        List<String> test = mDataManager.getPreferencesManager().loadUserProfileData();
+
         if (savedInstanceState == null) {
 
         } else {
-            mColorMode = savedInstanceState.getInt(ConstantManager.COLOR_MODE_KEY);
+            mCurrentEditMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeEditMode(mCurrentEditMode);
         }
     }
 
@@ -47,6 +87,21 @@ public class MainActivity extends AppCompatActivity{
             mNavigationDrawer.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                if (mCurrentEditMode == 0) {
+                    changeEditMode(1);
+                    mCurrentEditMode = 1;
+                } else {
+                    changeEditMode(0);
+                    mCurrentEditMode = 0;
+                }
+                break;
+        }
     }
 
     @Override
@@ -65,6 +120,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onPause() {
         super.onPause();
         LogUtils.d(TAG, "onPause");
+        saveUserInfoValue();
     }
 
     @Override
@@ -80,11 +136,10 @@ public class MainActivity extends AppCompatActivity{
     }
 
     @Override
-    protected  void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
         LogUtils.d(TAG, "onSaveInstanceState");
-        outState.putInt(ConstantManager.COLOR_MODE_KEY, mColorMode);
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrentEditMode);
     }
 
     private void setupToolBar() {
@@ -97,7 +152,56 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
-//    private void setupDrawer() {
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.navigatio_view);
-//    }
+    private void setupDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigator_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                ToastUtils.showShortMessage(item.toString(), getApplicationContext());
+                item.setCheckable(true);
+                mNavigationDrawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    /**
+     * переключает режим редактирования
+     *
+     * @param mode если 1 - режим редактирования, если 0 - режим просмотра
+     * @return
+     */
+    private void changeEditMode(int mode) {
+        if (mode == 1) {
+            mFab.setImageResource(R.drawable.ic_done_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+            }
+        } else {
+            mFab.setImageResource(R.drawable.ic_create_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setEnabled(false);
+                userValue.setFocusable(false);
+                userValue.setFocusableInTouchMode(false);
+                saveUserInfoValue();
+            }
+        }
+    }
+
+    private void loadUserInfoValue() {
+        List<String> userData = mDataManager.getPreferencesManager().loadUserProfileData();
+        for (int index = 0; index < userData.size(); index++) {
+            mUserInfoViews.get(index).setText(userData.get(index));
+        }
+    }
+
+    private void saveUserInfoValue() {
+        List<String> userData = new ArrayList<>();
+        for (EditText userFieldView : mUserInfoViews) {
+            userData.add(userFieldView.getText().toString());
+        }
+        mDataManager.getPreferencesManager().saveUserProfileData(userData);
+    }
 }
