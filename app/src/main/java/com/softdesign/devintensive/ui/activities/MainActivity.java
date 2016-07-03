e4e7d0508d526bd5d32dabc5a56178768406952b
bugtsa <bugtsa@gmail.com>
2016-07-03 12:41:56 +0300

    final lesson 4

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,16 +1,30 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.app.Dialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -25,11 +39,19 @@ import com.softdesign.devintensive.utils.ConstantManager;
 import com.softdesign.devintensive.utils.LogUtils;
 import com.softdesign.devintensive.utils.RoundedAvatarDrawable;
 import com.softdesign.devintensive.utils.ToastUtils;
+import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.jar.Manifest;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import static java.util.jar.Manifest.*;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
     private static final String TAG = ConstantManager.TAG_PREFIX + MainActivity.class.getSimpleName();
 
     private CoordinatorLayout mCoordinatorLayout;
@@ -46,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private FloatingActionButton mFab;
 
-    private ImageView mUserAvatar;
+    private ImageView mUserAvatarImage;
 
     private RelativeLayout mProfilePlaceHolder;
 
@@ -56,7 +78,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private AppBarLayout.LayoutParams mAppBarParams = null;
 
+    private ImageView mUserProfileImage;
+
     private int mCurrentEditMode = 0;
+
+    private File mPhotoFile = null;
+
+    private Uri mSelectedImage = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,8 +102,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mCollapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         mAppBarLayout = (AppBarLayout) findViewById(R.id.appbar_layout);
 
-        mUserAvatar = (ImageView) findViewById(R.id.user_avatar);
-        mUserAvatar.setImageBitmap(RoundedAvatarDrawable.getRoundedBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.user_avatar)));
+        mUserAvatarImage = (ImageView) findViewById(R.id.user_avatar_iv);
+        mUserAvatarImage.setImageBitmap(RoundedAvatarDrawable.getRoundedBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.user_avatar)));
+        mUserProfileImage = (ImageView) findViewById(R.id.user_profile_iv);
 
         mUserPhone = (EditText) findViewById(R.id.phone_et);
         mUserEmail = (EditText) findViewById(R.id.email_et);
@@ -89,16 +118,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mUserInfoViews.add(mUserVK);
         mUserInfoViews.add(mUserGit);
         mUserInfoViews.add(mUserBio);
+
+        mFab.setOnClickListener(this);
+        mProfilePlaceHolder.setOnClickListener(this);
+
+        setupToolBar();
+        setupDrawer();
         if (!isLoadUserInfoValue()) {
             initUserInfoValue();
         }
         loadUserInfoValue();
-
-        mFab.setOnClickListener(this);
-        setupToolBar();
-        setupDrawer();
-
-        List<String> test = mDataManager.getPreferencesManager().loadUserProfileData();
+        Picasso.with(this)
+                .load(mDataManager.getPreferencesManager().loadUserPhoto())
+                .placeholder(R.drawable.user_profile)
+                .into(mUserProfileImage);
 
         if (savedInstanceState == null) {
 
@@ -137,6 +170,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     mCurrentEditMode = 0;
                 }
                 break;
+            case R.id.profile_placeholder:
+                showDialog(ConstantManager.LOAD_PROFILE_PHOTO);
+                break;
         }
     }
 
@@ -173,13 +209,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /**
      * получает результат из другой Activity (фото из камеры или из галлереи)
+     *
      * @param requestCode
      * @param resultCode
      * @param data
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case ConstantManager.REQUEST_GALLERY_PICTURE:
+                if(resultCode == RESULT_OK && data != null) {
+                    mSelectedImage = data.getData();
 
+                    insertProfileImage(mSelectedImage);
+                }
+                break;
+            case ConstantManager.REQUEST_CAMERA_PICTURE:
+                if(resultCode ==RESULT_OK && mPhotoFile != null) {
+                    mSelectedImage = Uri.fromFile(mPhotoFile);
+
+                    insertProfileImage(mSelectedImage);
+                }
+        }
     }
 
     @Override
@@ -231,6 +282,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
             showProfilePlaceHolder();
             lockToolbar();
+            mCollapsingToolbar.setExpandedTitleColor(Color.TRANSPARENT);
         } else {
             mFab.setImageResource(R.drawable.ic_create_black_24dp);
             for (EditText userValue : mUserInfoViews) {
@@ -242,6 +294,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
             hideProfilePlaceHolder();
             unLockToolbar();
+            mCollapsingToolbar.setExpandedTitleColor(getResources().getColor(R.color.white));
         }
     }
 
@@ -259,9 +312,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         boolean isLoadUserInfo = false;
         List<String> userData = mDataManager.getPreferencesManager().loadUserProfileData();
         for (int index = 0; index < userData.size(); index++) {
-           if (!userData.get(index).isEmpty() && !userData.get(index).equals("null")) {
-               isLoadUserInfo = true;
-           }
+            if (!userData.get(index).isEmpty() && !userData.get(index).equals("null")) {
+                isLoadUserInfo = true;
+            }
         }
         return isLoadUserInfo;
     }
@@ -282,11 +335,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void loadPhotoFromGallery() {
+        Intent takeGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        takeGalleryIntent.setType("image/*");
+        startActivityForResult(Intent.createChooser(takeGalleryIntent, getString(R.string.chose_gallery_photo)), ConstantManager.REQUEST_GALLERY_PICTURE);
 
     }
 
     private void loadPhotoFromCamera() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Intent takeCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try {
+                mPhotoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+            if (mPhotoFile != null) {
+                takeCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPhotoFile));
+                startActivityForResult(takeCaptureIntent, ConstantManager.REQUEST_CAMERA_PICTURE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.CAMERA,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, ConstantManager.CAMERA_REQUEST_PERMISSION_CODE);
+            Snackbar.make(mCoordinatorLayout, R.string.give_permission_message, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.allow_message, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View  v) {
+                            openApplicationSettings();
+                        }
+                    }).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == ConstantManager.CAMERA_REQUEST_PERMISSION_CODE && grantResults.length == 2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            }
+        }
+
+        if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+        }
     }
 
     private void hideProfilePlaceHolder() {
@@ -304,7 +399,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void unLockToolbar() {
-        mAppBarParams.setScrollFlags(AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL| AppBarLayout.LayoutParams.SCROLL_FLAG_EXIT_UNTIL_COLLAPSED);
+        mAppBarParams.setScrollFlags(AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL | AppBarLayout.LayoutParams.SCROLL_FLAG_EXIT_UNTIL_COLLAPSED);
         mCollapsingToolbar.setLayoutParams(mAppBarParams);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case ConstantManager.LOAD_PROFILE_PHOTO:
+                String[] selectItems = {getString(R.string.user_profile_dialog_gallery), getString(R.string.user_profile_dialog_camera), getString(R.string.user_profile_dialog_cancel)};
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(getString(R.string.user_profile_placeholder_image_title));
+                builder.setItems(selectItems, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int choiceItem) {
+                        switch (choiceItem) {
+                            case 0:
+                                //загрузить из галлереи
+                                loadPhotoFromGallery();
+                                break;
+                            case 1:
+                                //загрузить из камеры
+                                loadPhotoFromCamera();
+                                break;
+                            case 2:
+                                //отмена
+                                dialog.cancel();
+                                break;
+                        }
+                    }
+                });
+                return builder.create();
+            default:
+                return null;
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.MediaColumns.DATA, image.getAbsolutePath());
+
+        this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        return image;
+    }
+
+    private void insertProfileImage(Uri selectedImage) {
+        Picasso.with(this)
+                .load(selectedImage)
+                .into(mUserProfileImage);
+        mDataManager.getPreferencesManager().saveUserPhoto(selectedImage);
+    }
+
+    public void openApplicationSettings() {
+        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+
+        startActivityForResult(appSettingsIntent, ConstantManager.PERMISSION_REQUEST_SETTINGS_CODE);
     }
 }
--- app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -10,4 +10,11 @@ public interface ConstantManager {
     String USER_VK_KEY = "USER_VK_KEY";
     String USER_GIT_KEY = "USER_GIT_KEY";
     String USER_BIO_KEY = "USER_BIO_KEY";
+    String USER_PHOTO_KEY = "USER_PHOTO_KEY";
+
+    int LOAD_PROFILE_PHOTO = 1;
+    int REQUEST_CAMERA_PICTURE = 77;
+    int REQUEST_GALLERY_PICTURE = 88;
+    int PERMISSION_REQUEST_SETTINGS_CODE = 99;
+    int CAMERA_REQUEST_PERMISSION_CODE = 111;
 }
