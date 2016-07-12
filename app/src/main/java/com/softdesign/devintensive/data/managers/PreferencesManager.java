eaef338b7dcd7f524db52f6d7e6905eef6d83855
bugtsa <bugtsa@gmail.com>
2016-07-12 21:37:28 +0300

    Task 1 and 2

--- app/src/main/java/com/softdesign/devintensive/data/managers/PreferencesManager.java
+++ app/src/main/java/com/softdesign/devintensive/data/managers/PreferencesManager.java
@@ -14,7 +14,13 @@ public class PreferencesManager {
     private SharedPreferences mSharedPreferences;
 
     private static final String[] USER_FIELDS = {ConstantManager.USER_PHONE_KEY, ConstantManager.USER_EMAIL_KEY,
-                                                ConstantManager.USER_VK_KEY, ConstantManager.USER_GIT_KEY, ConstantManager.USER_BIO_KEY};
+            ConstantManager.USER_VK_KEY, ConstantManager.USER_GIT_KEY, ConstantManager.USER_BIO_KEY};
+
+    private static final String[] USER_VALUES = {
+            ConstantManager.USER_RATING_VALUE,
+            ConstantManager.USER_CODE_LINES_VALUE,
+            ConstantManager.USER_PROJECT_VALUE
+    };
 
     /**
      * Присваивает SharedPreferences в переменную класса
@@ -25,6 +31,7 @@ public class PreferencesManager {
 
     /**
      * Сохраняет пользовательскую информацию в SharedPreferences
+     *
      * @param userFields List строк полей пользовательской информации
      */
     public void saveUserProfileData(List<String> userFields) {
@@ -38,6 +45,7 @@ public class PreferencesManager {
 
     /**
      * Загружает пользовательскую информацию из SharedPreferences в интерфейс приложения
+     *
      * @return List строк полей пользовательской информации
      */
     public List<String> loadUserProfileData() {
@@ -52,6 +60,7 @@ public class PreferencesManager {
 
     /**
      * Сохраняет изображение профиля пользователя
+     *
      * @param uri изображения профиля пользователя
      */
     public void saveUserPhoto(Uri uri) {
@@ -62,9 +71,73 @@ public class PreferencesManager {
 
     /**
      * Загружает изображение профиля пользователя
+     *
      * @return изображение профиля пользователя
      */
     public Uri loadUserPhoto() {
         return Uri.parse(mSharedPreferences.getString(ConstantManager.USER_PHOTO_KEY, "android.resource://com.softdesign.devintensive/drawable/user_profile"));
+    }
+
+    public void saveUserProfileValues(int[] userValues) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+
+        for (int i = 0; i < USER_VALUES.length; i++) {
+            editor.putString(USER_VALUES[i], String.valueOf(userValues[i]));
+        }
+        editor.apply();
+    }
+
+    public List<String> loadUserProfileValues() {
+        List<String> userValues = new ArrayList<>();
+        userValues.add(mSharedPreferences.getString(ConstantManager.USER_RATING_VALUE, "0"));
+        userValues.add(mSharedPreferences.getString(ConstantManager.USER_CODE_LINES_VALUE, "0"));
+        userValues.add(mSharedPreferences.getString(ConstantManager.USER_PROJECT_VALUE, "0"));
+
+        return userValues;
+    }
+
+    public void saveUserAvatar(Uri uri) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.USER_AVATAR_KEY, uri.toString());
+        editor.apply();
+    }
+
+    public Uri loadUserAvatar() {
+        return Uri.parse(mSharedPreferences.getString(ConstantManager.USER_AVATAR_KEY,
+                "android.resource://com.softdesign.devintensive/drawable/avatar_bg"));
+    }
+
+    public void saveAuthToken(String authToken) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.AUTH_TOKEN_KEY, authToken);
+        editor.apply();
+    }
+
+    public String getAuthToken() {
+        return mSharedPreferences.getString(ConstantManager.AUTH_TOKEN_KEY, "");
+    }
+
+    public void saveUserId(String userId) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.USER_ID_KEY, userId);
+        editor.apply();
+    }
+
+    public String getUserId() {
+        return mSharedPreferences.getString(ConstantManager.USER_ID_KEY, "");
+    }
+
+    public void saveUserFullName(String userFullName) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(ConstantManager.USER_FULL_NAME_KEY, userFullName);
+        editor.apply();
+    }
+
+    public String getUserFullName() {
+        return mSharedPreferences.getString(ConstantManager.USER_FULL_NAME_KEY, "");
+    }
+
+    public String getUserEmail() {
+        return mSharedPreferences.getString(ConstantManager.EDIT_MAIL_KEY, "");
     }
 }
--- app/src/main/java/com/softdesign/devintensive/ui/activities/AuthActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/AuthActivity.java
@@ -1,15 +1,123 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.data.managers.DataManager;
+import com.softdesign.devintensive.data.network.req.UserLoginReq;
+import com.softdesign.devintensive.data.network.res.UserModelRes;
+import com.softdesign.devintensive.utils.ConstantManager;
+import com.softdesign.devintensive.utils.NetworkStatusChecker;
 
-public class AuthActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    @Override
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AuthActivity extends BaseActivity{
+    private static final String TAG = ConstantManager.TAG_PREFIX + "AuthActivity";
+
+    private DataManager mDataManager;
+
+    @BindView(R.id.login_email_et)
+    EditText mAuthLogin;
+    @BindView(R.id.login_password_et)
+    EditText mAuthPass;
+    @BindView(R.id.login_btn)
+    Button mAuthLoginBtn;
+    @BindView(R.id.remember_tv)
+    TextView mRememberPass;
+    @BindView(R.id.main_coordinator_auth)
+    CoordinatorLayout mCoordinatorLayout;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        ButterKnife.bind(this);
+
+        mDataManager = DataManager.getInstance();
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    @OnClick(R.id.remember_tv)
+    protected void rememberPass() {
+        Intent rememberIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(ConstantManager.FORGOT_PASSWORD));
+        startActivity(rememberIntent);
+    }
+
+    @OnClick(R.id.login_btn)
+    protected void signIn() {
+            if (NetworkStatusChecker.isNetworkAvailable(this)) {
+                Call<UserModelRes> call = mDataManager.loginUser(new UserLoginReq(mAuthLogin.getText().toString(), mAuthPass.getText().toString()));
+                call.enqueue(new Callback<UserModelRes>() {
+                    @Override
+                    public void onResponse(Call<UserModelRes> call, Response<UserModelRes> response) {
+                        if (response.code() == 200) {
+                            loginSuccess(response.body());
+                        } else if (response.code() == 404) {
+                            showSnackBar(getString(R.string.login_password_not_match_response));
+                        } else {
+                            showSnackBar(getString(R.string.not_known_response));
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<UserModelRes> call, Throwable t) {
+                        showSnackBar(getString(R.string.error_response) + t.getMessage());
+                    }
+                });
+            } else {
+                showSnackBar(getString(R.string.network_not_access_response));
+            }
+    }
+
+    protected void loginSuccess(UserModelRes userModel) {
+        showSnackBar(userModel.getData().getToken());
+        mDataManager.getPreferencesManager().saveAuthToken(userModel.getData().getToken());
+        mDataManager.getPreferencesManager().saveUserId(userModel.getData().getUser().getId());
+        mDataManager.getPreferencesManager().saveUserPhoto(Uri.parse(userModel.getData().getUser().getPublicInfo().getPhoto()));
+        mDataManager.getPreferencesManager().saveUserAvatar(Uri.parse(userModel.getData().getUser().getPublicInfo().getAvatar()));
+        mDataManager.getPreferencesManager().saveUserFullName(userModel.getData().getUser().getFirstName() + " " + userModel.getData().getUser().getSecondName());
+        saveUserValues(userModel);
+        saveUserData(userModel);
+
+        Intent loginIntent = new Intent(this, MainActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void saveUserValues(UserModelRes userModel) {
+        int[] userValues = {
+                userModel.getData().getUser().getProfileValues().getRaiting(),
+                userModel.getData().getUser().getProfileValues().getLinesCode(),
+                userModel.getData().getUser().getProfileValues().getProjects()
+        };
+
+        mDataManager.getPreferencesManager().saveUserProfileValues(userValues);
+    }
+
+    private void saveUserData(UserModelRes userModel) {
+        List<String> userData = new ArrayList<>();
+        userData.add(userModel.getData().getUser().getContacts().getPhone());
+        userData.add(userModel.getData().getUser().getContacts().getEmail());
+        userData.add(userModel.getData().getUser().getContacts().getVk());
+        userData.add(userModel.getData().getUser().getRepositories().getRepo().get(0).getGit());
+        userData.add(userModel.getData().getUser().getPublicInfo().getBio());
+
+        mDataManager.getPreferencesManager().saveUserProfileData(userData);
     }
 }
